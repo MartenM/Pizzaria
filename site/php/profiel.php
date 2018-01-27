3b60cace28fbcb8f03279b079e19e78597435d8a
MartenM <marten.struijk@gmail.com>
2018-01-27 11:39:08 +0100

    Minor veranderingen
    DateTime is nu TIMESTAMP om er voor te zorgen dat het op lagere MySql versies werkt.

--- site/php/profiel.php
+++ site/php/profiel.php
@@ -17,6 +17,7 @@
     
     <link rel="stylesheet" href="..\theme.css">
     <link rel="stylesheet" href="..\animate.css">
+    <link rel="stylesheet" href="profielpagina.css">
     
     <!-- Latest compiled and minified CSS van boostrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
