51108170464a5a57ecfc98a840787571c3b10038
MartenM <marten.struijk@gmail.com>
2018-01-27 22:38:50 +0100

    Finished
    Profiel afgemaakt.

--- site/php/profiel.php
+++ site/php/profiel.php
@@ -5,6 +5,12 @@
 
     $notify_msg = '';
 
+    // Laad het database php script.
+    $mysqli = '';
+    require 'db.php';
+
+    $sql = "SELECT * FROM bestellingen WHERE klant=" . $_SESSION['id'] . " ORDER BY datum DESC";
+    $geschiedenis = $mysqli->query($sql);
 
 ?>
 
@@ -66,19 +72,70 @@
             <div class="profiel">
                 <div class="naam">
                     <h2>Welkom terug,</h2>
-                    <h4><?php echo $_SESSION['voornaam'] . ' ' . $_SESSION['achternaam'] ?></h4>
+                    <span><?php echo $_SESSION['voornaam'] . ' ' . $_SESSION['achternaam']; ?></span>
+                    <span class="id">#<?php echo $_SESSION['id']; ?></span>
                 </div>
 
-                <div class="stats">
-                    <ul>
-                        <li><img src="https://cdn4.iconfinder.com/data/icons/pictograms-1/512/Calendar-512.png" style="height: 16px; width: 16px"> <span class="key">Geregistreerd op:</span> <span class="value"><?php echo $_SESSION['registratie'];?></span></li>
-                        <li><img src="https://services.garmin.com/appsLibraryBusinessServices_v0/rest/apps/da79ef76-eb92-48ba-a360-dd4dd1519b03/icon/30162724-ed8b-4a46-93ca-37c4d4ab8d45" style="height: 16px; width: 16px"> <span class="key">Aantal bestellingen:</span> <span class="value"><?php echo $_SESSION['bestellingen'];?></span></li>
-                        <li><img src="http://www.iconarchive.com/download/i99744/sonya/swarm/Pizza.ico" style="height: 16px; width: 16px"> <span class="key">Spaarpunten:</span> <span class="value"><?php echo $_SESSION['spaarpunten'];?></span></li>
-                    </ul>
+                <div class="menu clearfix">
+                    <div class="stats">
+                        <ul>
+                            <li><img src="https://cdn4.iconfinder.com/data/icons/pictograms-1/512/Calendar-512.png" style="height: 16px; width: 16px"> <span class="key">Geregistreerd op:</span> <span class="value"><?php echo $_SESSION['registratie'];?></span></li>
+                            <li><img src="https://services.garmin.com/appsLibraryBusinessServices_v0/rest/apps/da79ef76-eb92-48ba-a360-dd4dd1519b03/icon/30162724-ed8b-4a46-93ca-37c4d4ab8d45" style="height: 16px; width: 16px"> <span class="key">Aantal bestellingen:</span> <span class="value"><?php echo $_SESSION['bestellingen'];?></span></li>
+                            <li><img src="http://www.iconarchive.com/download/i99744/sonya/swarm/Pizza.ico" style="height: 16px; width: 16px"> <span class="key">Spaarpunten:</span> <span class="value"><?php echo $_SESSION['spaarpunten'];?></span></li>
+                        </ul>
+                    </div>
+                    <div class="bestel">
+                        <form action="bestellen.php" method="get">
+                            <button type="submit" id="fbutton"><img style="height: 16px; width: 16px" src="http://wfarm2.dataknet.com/static/resources/icons/set112/54979173.png"> Nieuwe bestelling</button>
+                        </form>
+                    </div>
                 </div>
+                
 
                 <div class="geschiedenis">
+                    <h3>Geschiedenis</h3>
+                    <div class="scroll">
+                        <table>
+                            <tr>
+                                <th>Bestelling</th>
+                                <th>Afhalen</th>
+                                <th>Adres</th>
+                                <th>Prijs</th>
+                                <th>Datum</th>
+                                <th>Afgehandeld</th>
+                            </tr>
+                            <?php
+                            
+                                if($geschiedenis->num_rows == 0){
+                                echo "<span style='color: green'>U heeft nog geen bestellingen geplaats.</span>";
+                                } else {
+                            
+                                    while($rij = $geschiedenis->fetch_assoc()) {
+                                        echo '<tr>';
+                                
+                                        echo "<td>" . $rij['bestelling'] . "</td>";
+                                        if($rij['afhalen'] == '0'){
+                                            echo "<td>" . "<span>Nee</span>" . "</td>";
+                                        } else{
+                                            echo "<td>" . "<span>Ja</span>" . "</td>";
+                                        }
+                                        
+                                        echo "<td>" . $rij['adres'] . "</td>";
+                                        echo "<td>" . $rij['totaalprijs'] . "</td>";
+                                        echo "<td>" . $rij['datum'] . "</td>";
 
+                                        if($rij['afgehandeld'] == '0'){
+                                            echo "<td>" . "<span class='nee'>Nee</span>" . "</td>";
+                                        } else{
+                                            echo "<td>" . "<span class='ja'>Ja</span>" . "</td>";
+                                        }
+                                
+                                        echo '</tr>';
+                                    }
+                                }
+                            ?>
+                        </table>
+                    </div>
                 </div>
 
             </div>
