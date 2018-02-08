ad3b96aa476ee2d3b42b91403811a3950d71cb36
MartenM <marten.struijk@gmail.com>
2018-02-08 15:43:30 +0100

    Added base connection

--- visualbasic/PizzeriaApplicatie/PizzeriaApplicatie/Form1.Designer.vb
+++ visualbasic/PizzeriaApplicatie/PizzeriaApplicatie/Form1.Designer.vb
@@ -25,6 +25,12 @@ Partial Class Form1
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.sidePanel = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.BT_ZoekKlant = New System.Windows.Forms.Button()
+        Me.BT_ZoekBestelling = New System.Windows.Forms.Button()
+        Me.BT_OpenBestellingen = New System.Windows.Forms.Button()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.logoPanel = New System.Windows.Forms.Panel()
         Me.Labl_Logo = New System.Windows.Forms.Label()
         Me.header = New System.Windows.Forms.Panel()
@@ -40,27 +46,114 @@ Partial Class Form1
         Me.TB_Bestelling = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Panel3 = New System.Windows.Forms.Panel()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Label_UpdateIn = New System.Windows.Forms.Label()
         Me.label_tijd = New System.Windows.Forms.Label()
+        Me.Label_UpdateIn = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.UpdateTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.OptiesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OverToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.sidePanel.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.Panel4.SuspendLayout()
         Me.logoPanel.SuspendLayout()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
         Me.Panel3.SuspendLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'sidePanel
         '
         Me.sidePanel.BackColor = System.Drawing.Color.Silver
+        Me.sidePanel.Controls.Add(Me.Panel5)
+        Me.sidePanel.Controls.Add(Me.Panel4)
         Me.sidePanel.Controls.Add(Me.logoPanel)
         Me.sidePanel.Dock = System.Windows.Forms.DockStyle.Left
-        Me.sidePanel.Location = New System.Drawing.Point(0, 0)
+        Me.sidePanel.Location = New System.Drawing.Point(0, 24)
         Me.sidePanel.Name = "sidePanel"
-        Me.sidePanel.Size = New System.Drawing.Size(186, 461)
+        Me.sidePanel.Size = New System.Drawing.Size(186, 479)
         Me.sidePanel.TabIndex = 1
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.White
+        Me.Panel5.Controls.Add(Me.BT_ZoekKlant)
+        Me.Panel5.Controls.Add(Me.BT_ZoekBestelling)
+        Me.Panel5.Controls.Add(Me.BT_OpenBestellingen)
+        Me.Panel5.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel5.Location = New System.Drawing.Point(0, 90)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(186, 280)
+        Me.Panel5.TabIndex = 2
+        '
+        'BT_ZoekKlant
+        '
+        Me.BT_ZoekKlant.BackColor = System.Drawing.Color.FromArgb(CType(CType(81, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(76, Byte), Integer))
+        Me.BT_ZoekKlant.Dock = System.Windows.Forms.DockStyle.Top
+        Me.BT_ZoekKlant.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BT_ZoekKlant.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_ZoekKlant.ForeColor = System.Drawing.Color.White
+        Me.BT_ZoekKlant.Location = New System.Drawing.Point(0, 80)
+        Me.BT_ZoekKlant.Margin = New System.Windows.Forms.Padding(0)
+        Me.BT_ZoekKlant.Name = "BT_ZoekKlant"
+        Me.BT_ZoekKlant.Size = New System.Drawing.Size(186, 40)
+        Me.BT_ZoekKlant.TabIndex = 9
+        Me.BT_ZoekKlant.Text = "Zoek klant"
+        Me.BT_ZoekKlant.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.BT_ZoekKlant.UseVisualStyleBackColor = False
+        '
+        'BT_ZoekBestelling
+        '
+        Me.BT_ZoekBestelling.BackColor = System.Drawing.Color.FromArgb(CType(CType(81, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(76, Byte), Integer))
+        Me.BT_ZoekBestelling.Dock = System.Windows.Forms.DockStyle.Top
+        Me.BT_ZoekBestelling.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BT_ZoekBestelling.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_ZoekBestelling.ForeColor = System.Drawing.Color.White
+        Me.BT_ZoekBestelling.Location = New System.Drawing.Point(0, 40)
+        Me.BT_ZoekBestelling.Margin = New System.Windows.Forms.Padding(0)
+        Me.BT_ZoekBestelling.Name = "BT_ZoekBestelling"
+        Me.BT_ZoekBestelling.Size = New System.Drawing.Size(186, 40)
+        Me.BT_ZoekBestelling.TabIndex = 8
+        Me.BT_ZoekBestelling.Text = "Zoek bestelling"
+        Me.BT_ZoekBestelling.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.BT_ZoekBestelling.UseVisualStyleBackColor = False
+        '
+        'BT_OpenBestellingen
+        '
+        Me.BT_OpenBestellingen.BackColor = System.Drawing.Color.FromArgb(CType(CType(81, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(76, Byte), Integer))
+        Me.BT_OpenBestellingen.Dock = System.Windows.Forms.DockStyle.Top
+        Me.BT_OpenBestellingen.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BT_OpenBestellingen.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BT_OpenBestellingen.ForeColor = System.Drawing.Color.White
+        Me.BT_OpenBestellingen.Location = New System.Drawing.Point(0, 0)
+        Me.BT_OpenBestellingen.Margin = New System.Windows.Forms.Padding(0)
+        Me.BT_OpenBestellingen.Name = "BT_OpenBestellingen"
+        Me.BT_OpenBestellingen.Size = New System.Drawing.Size(186, 40)
+        Me.BT_OpenBestellingen.TabIndex = 7
+        Me.BT_OpenBestellingen.Text = "Open bestellingen"
+        Me.BT_OpenBestellingen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.BT_OpenBestellingen.UseVisualStyleBackColor = False
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.Label3)
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel4.Location = New System.Drawing.Point(0, 60)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(186, 30)
+        Me.Panel4.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(3, 7)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Grid opties"
         '
         'logoPanel
         '
@@ -87,9 +180,9 @@ Partial Class Form1
         '
         Me.header.BackColor = System.Drawing.Color.FromArgb(CType(CType(81, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(76, Byte), Integer))
         Me.header.Dock = System.Windows.Forms.DockStyle.Top
-        Me.header.Location = New System.Drawing.Point(186, 0)
+        Me.header.Location = New System.Drawing.Point(186, 24)
         Me.header.Name = "header"
-        Me.header.Size = New System.Drawing.Size(657, 60)
+        Me.header.Size = New System.Drawing.Size(706, 60)
         Me.header.TabIndex = 2
         '
         'Panel1
@@ -103,9 +196,9 @@ Partial Class Form1
         Me.Panel1.Controls.Add(Me.TB_Bestelling)
         Me.Panel1.Controls.Add(Me.Label1)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Right
-        Me.Panel1.Location = New System.Drawing.Point(644, 60)
+        Me.Panel1.Location = New System.Drawing.Point(693, 84)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(199, 401)
+        Me.Panel1.Size = New System.Drawing.Size(199, 419)
         Me.Panel1.TabIndex = 3
         '
         'Panel2
@@ -115,7 +208,7 @@ Partial Class Form1
         Me.Panel2.Controls.Add(Me.BT_Cancel)
         Me.Panel2.Controls.Add(Me.BT_Afhandelen)
         Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.Panel2.Location = New System.Drawing.Point(0, 190)
+        Me.Panel2.Location = New System.Drawing.Point(0, 208)
         Me.Panel2.Name = "Panel2"
         Me.Panel2.Size = New System.Drawing.Size(199, 211)
         Me.Panel2.TabIndex = 9
@@ -228,19 +321,19 @@ Partial Class Form1
         Me.Panel3.Controls.Add(Me.label_tijd)
         Me.Panel3.Controls.Add(Me.Label_UpdateIn)
         Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
-        Me.Panel3.Location = New System.Drawing.Point(186, 60)
+        Me.Panel3.Location = New System.Drawing.Point(186, 84)
         Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(458, 30)
+        Me.Panel3.Size = New System.Drawing.Size(507, 30)
         Me.Panel3.TabIndex = 4
         '
-        'DataGridView1
+        'label_tijd
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DataGridView1.Location = New System.Drawing.Point(186, 90)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(458, 371)
-        Me.DataGridView1.TabIndex = 5
+        Me.label_tijd.AutoSize = True
+        Me.label_tijd.Location = New System.Drawing.Point(77, 7)
+        Me.label_tijd.Name = "label_tijd"
+        Me.label_tijd.Size = New System.Drawing.Size(16, 13)
+        Me.label_tijd.TabIndex = 1
+        Me.label_tijd.Text = "..."
         '
         'Label_UpdateIn
         '
@@ -252,35 +345,64 @@ Partial Class Form1
         Me.Label_UpdateIn.TabIndex = 0
         Me.Label_UpdateIn.Text = "Update in:"
         '
-        'label_tijd
+        'DataGridView1
         '
-        Me.label_tijd.AutoSize = True
-        Me.label_tijd.Location = New System.Drawing.Point(77, 7)
-        Me.label_tijd.Name = "label_tijd"
-        Me.label_tijd.Size = New System.Drawing.Size(32, 13)
-        Me.label_tijd.TabIndex = 1
-        Me.label_tijd.Text = "<tijd>"
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(186, 114)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(507, 389)
+        Me.DataGridView1.TabIndex = 5
         '
         'UpdateTimer
         '
         Me.UpdateTimer.Enabled = True
         Me.UpdateTimer.Interval = 1000
         '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OptiesToolStripMenuItem, Me.OverToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(892, 24)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'OptiesToolStripMenuItem
+        '
+        Me.OptiesToolStripMenuItem.Name = "OptiesToolStripMenuItem"
+        Me.OptiesToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.OptiesToolStripMenuItem.Text = "Opties"
+        '
+        'OverToolStripMenuItem
+        '
+        Me.OverToolStripMenuItem.Name = "OverToolStripMenuItem"
+        Me.OverToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.OverToolStripMenuItem.Text = "Over"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(843, 461)
+        Me.ClientSize = New System.Drawing.Size(892, 503)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.header)
         Me.Controls.Add(Me.sidePanel)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MinimumSize = New System.Drawing.Size(550, 500)
         Me.Name = "Form1"
         Me.Text = "Pizzeria Applicatie"
         Me.sidePanel.ResumeLayout(False)
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
         Me.logoPanel.ResumeLayout(False)
         Me.logoPanel.PerformLayout()
         Me.Panel1.ResumeLayout(False)
@@ -289,7 +411,10 @@ Partial Class Form1
         Me.Panel3.ResumeLayout(False)
         Me.Panel3.PerformLayout()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
@@ -313,4 +438,13 @@ Partial Class Form1
     Friend WithEvents Label_UpdateIn As Label
     Friend WithEvents DataGridView1 As DataGridView
     Friend WithEvents UpdateTimer As Timer
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents BT_OpenBestellingen As Button
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BT_ZoekKlant As Button
+    Friend WithEvents BT_ZoekBestelling As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents OptiesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OverToolStripMenuItem As ToolStripMenuItem
 End Class
--- visualbasic/PizzeriaApplicatie/PizzeriaApplicatie/Form1.vb
+++ visualbasic/PizzeriaApplicatie/PizzeriaApplicatie/Form1.vb
@@ -1,11 +1,47 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
     Dim tijd As Integer = 120
+    Dim database As DrlDatabase
 
+    Public Sub updateDataGridView(ByVal query As String)
+        label_tijd.Text = "Query word uitgevoerd: " + query
 
+        Dim connection As MySqlConnection = database.grabConnection()
+        Try
+            connection.Open()
+
+            Dim adapter As New MySqlDataAdapter(query, connection)
+            Dim dataset As New DataSet
+
+            adapter.Fill(dataset)
+
+            DataGridView1.DataSource = dataset.Tables(0)
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " + ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub Label_UpdateIn_Click(sender As Object, e As EventArgs) Handles Label_UpdateIn.Click
+        tijd = 0
+    End Sub
+
+    ' Functie die word uitgevoerd wanneer Form1 (Mainform) opend.
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Hier zorgen we er voor dat het database object present is en dat alle gegevens er in worden geladen.
+        database = New DrlDatabase
+        database.adres = My.Settings.db_adres
+        database.username = My.Settings.db_username
+        database.wachtwoord = My.Settings.db_wachtwoord
+        database.database = My.Settings.db_database
+    End Sub
+
+    ' Functie die word uitgevoerd wanneer de timer 1000ms is verstreken.
     Private Sub UpdateTimer_Tick(sender As Object, e As EventArgs) Handles UpdateTimer.Tick
         If (tijd = 0) Then
-            updateDataGridView()
             tijd = 120
+            updateDataGridView("select * from bestellingen")
         Else
             tijd -= 1
             label_tijd.Text = tijd
@@ -16,13 +52,5 @@
                 label_tijd.ForeColor = Color.Orange
             End If
         End If
-    End Sub
-
-    Sub updateDataGridView()
-        label_tijd.Text = "Update!"
-    End Sub
-
-    Private Sub Label_UpdateIn_Click(sender As Object, e As EventArgs) Handles Label_UpdateIn.Click
-        tijd = 0
     End Sub
 End Class
